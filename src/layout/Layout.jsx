1bc6c22f195f42297245037fc9f3d64354644e97
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-02-24 03:15:18 +0330

    responsive layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Layout({children}) {
   return (
     <>
-    <header className='w-[1000px]  p-6 mb-[80px] box-border text-white   bg-[#303030] rounded-2xl flex justify-between  items-center'>
+    <header className=' w-[100%]  p-6 mb-[80px] box-border text-white   bg-[#303030] rounded-2xl flex justify-between  items-center'>
         <h3 className='text-3xl'>Book App</h3>
         <p>
             <a href="https://github.com/Mani-Jebraeily"
@@ -14,7 +14,7 @@ function Layout({children}) {
     </header>
     {children}
 
-    <footer className='w-[1000px]  p-5 text-white  mt-20 box-border   bg-[#303030] rounded-2xl flex justify-center  items-center'>
+    <footer className='w-[100%]  p-5 text-white  mt-20 box-border   bg-[#303030] rounded-2xl flex justify-center  items-center'>
         <p className=''>Developed by Mani with ❤️</p>
     </footer>
     </>
